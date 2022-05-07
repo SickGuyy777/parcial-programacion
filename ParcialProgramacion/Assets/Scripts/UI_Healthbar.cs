6bcb063db6d3074be0b8135ec81b57af0b2f96e7
Nanox42 <nahuelstagno@gmail.com>
2022-05-07 19:11:38 -0300

    hud

--- ParcialProgramacion/Assets/Scripts/UI_Healthbar.cs
+++ ParcialProgramacion/Assets/Scripts/UI_Healthbar.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Healthbar : MonoBehaviour
+public class UI_Healthbar : empty
 {
-    public Image fillImage;
-    public void UpdateHealth(int currentHealth, int baseHealth)
+    public new int basehealth;
+    public new int CurrentHealth;
+    public Image ColorVida;
+
+    public void RevisarVida()
     {
-        fillImage.fillAmount = (float)currentHealth / (float)baseHealth;
+        ColorVida.fillAmount = basehealth / CurrentHealth;
     }
+
+    //public Image ColorVida;
+    //public void UpdateHealth(int currentHealth, int baseHealth)
+    //{
+    //  ColorVida.fillAmount = (float)currentHealth / (float)baseHealth;
+
+
+
+    //}
 }
